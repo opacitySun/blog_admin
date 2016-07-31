5ac2caa1d44919b00d89d6f54e608e2a2d752b30
opacitySun <809340810@qq.com>
2016-07-31 15:17:06 +0800

    更新修改

--- public/javascripts/app/controller/c-menu.js
+++ public/javascripts/app/controller/c-menu.js
@@ -6,12 +6,16 @@ define(['./Base'], function (Base) {
         		if($(this).hasClass("children")){
         			$(this).parents("#menuUl").find("li").removeClass("active");
         			$(this).addClass("active");
-
-        			var pageTitle = $(this).parent().parent().find(".page_title").text();
-        			var pageSmall = $(this).find("a").text();
-        			$("#pageHeader").html(pageTitle+"<small>"+pageSmall+"</small>");
+        			var pageHref = $(this).find("a").attr("page-href");
+        			//window.location.href = pageHref;
         		}
         	});
+        },
+        //改变页面标题
+        updateTitle : function(){
+        	var pageTitle = $("#menuUl li.active").parent().parent().find(".page_title").text();
+			var pageSmall = $("#menuUl li.active").find("a").text();
+			$("#pageHeader").html(pageTitle+"<small>"+pageSmall+"</small>");
         }
     };
 
--- public/javascripts/app/main-menu.js
+++ public/javascripts/app/main-menu.js
@@ -1,4 +1,6 @@
 define(['jquery','fnbase','bootstrap','./controller/c-menu','./model/m-menu'],function($,fnbase,bootstrap,controller,model){
 	//添加选中样式
     controller.liActive();
+    //改变页面标题
+    controller.updateTitle();
 });
