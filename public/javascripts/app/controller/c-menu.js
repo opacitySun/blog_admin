58fe78d79ac3bb9657db6627a88cad42d9f13ccd
opacitySun <809340810@qq.com>
2016-07-31 15:23:23 +0800

    更新修改

--- public/javascripts/app/controller/c-menu.js
+++ public/javascripts/app/controller/c-menu.js
@@ -2,20 +2,21 @@ define(['./Base'], function (Base) {
     var cMenu = {
     	//添加选中样式
         liActive : function(){
-        	$("#menuUl").on("li","click",function(){
-        		if($(this).hasClass("children")){
-        			$(this).parents("#menuUl").find("li").removeClass("active");
-        			$(this).addClass("active");
-        			var pageHref = $(this).find("a").attr("page-href");
-        			//window.location.href = pageHref;
-        		}
+        	$("#menuUl li.children").on("click",function(){
+    			$(this).parents("#menuUl").find("li").removeClass("active");
+    			$(this).addClass("active");
+    			var pageHref = $(this).find("a").attr("page-href");
+    			//window.location.href = pageHref;
         	});
         },
         //改变页面标题
         updateTitle : function(){
-        	var pageTitle = $("#menuUl li.active").parent().parent().find(".page_title").text();
-			var pageSmall = $("#menuUl li.active").find("a").text();
-			$("#pageHeader").html(pageTitle+"<small>"+pageSmall+"</small>");
+        	var liActiveLen = $("#menuUl li.active").length;
+        	if(liActiveLen > 0){
+        		var pageTitle = $("#menuUl li.active").parent().parent().find(".page_title").text();
+				var pageSmall = $("#menuUl li.active").find("a").text();
+				$("#pageHeader").html(pageTitle+"<small>"+pageSmall+"</small>");
+        	}
         }
     };
 
