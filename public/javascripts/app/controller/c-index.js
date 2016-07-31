6cb824464730a107f288b610fa493843764687a8
opacitySun <809340810@qq.com>
2016-07-31 18:10:41 +0800

    更新修改

--- public/javascripts/app/controller/c-index.js
+++ public/javascripts/app/controller/c-index.js
@@ -3,12 +3,14 @@ define(["./Base"],function(Base){
 	var cIndex = {
 		//根据banner类型判断添加按钮是否显示
 		addImgButtonShowByBannerType : function(){
-			var bannerTypeVal = $("input[name='bannerType']").val();
-			if(bannerTypeVal == 0){	//单张图
-				$(".image_upload span").hide();
-			}else{	//轮播图
-				$(".image_upload span").show();
-			}
+			$("input[name='bannerType']").on("click",function(){
+				var bannerTypeVal = $("input[name='bannerType']").val();
+				if(bannerTypeVal == 0){	//单张图
+					$(".image_upload span").hide();
+				}else{	//轮播图
+					$(".image_upload span").show();
+				}
+			});
 		}
 	};
 
