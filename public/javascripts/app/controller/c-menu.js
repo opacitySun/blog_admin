b8787a1bec572348b1ba353293cc3d7f80443e63
opacitySun <809340810@qq.com>
2016-07-31 15:29:45 +0800

    更新修改

--- public/javascripts/app/controller/c-menu.js
+++ public/javascripts/app/controller/c-menu.js
@@ -5,6 +5,9 @@ define(['./Base'], function (Base) {
         	$("#menuUl li.children").on("click",function(){
     			$(this).parents("#menuUl").find("li").removeClass("active");
     			$(this).addClass("active");
+    			var pageTitle = $(this).parent().parent().find(".page_title").text();
+				var pageSmall = $(this).find("a").text();
+				$("#pageHeader").html(pageTitle+"<small>"+pageSmall+"</small>");
     			var pageHref = $(this).find("a").attr("page-href");
     			window.location.href = "#";
         	});
