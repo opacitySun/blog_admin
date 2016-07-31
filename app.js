6e6b81049d2818557b9617e30f70d4705fbf830b
opacitySun <809340810@qq.com>
2016-07-31 14:46:57 +0800

    更新修改

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.set('views', __dirname + '/views');
 app.engine('.html',ejs.__express);
 app.set('view engine', 'html');
 app.use(partials());
-app.use(nodexcn());
 /*
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
