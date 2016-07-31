e777620c6fc75b221642bdb7d753f998c1c912e2
opacitySun <809340810@qq.com>
2016-07-31 18:08:38 +0800

    更新修改

--- public/javascripts/app/main-index.js
+++ public/javascripts/app/main-index.js
@@ -2,7 +2,7 @@
 define(['jquery','fnbase','bootstrap','./controller/c-index','./model/m-index'],function($,fnbase,bootstrap,controller,model){
 	var url = window.location.href;
 	if(fnbase.inString("/index-banner",url)){	//banner管理
-		
+		controller.addImgButtonShowByBannerType();
 	}else{	//欢迎页
 		$("#pageHeader").html("SUN Admin <small>欢迎来到孙博为的博客管理</small>");
 	}
