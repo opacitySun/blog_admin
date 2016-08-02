24e9510405a6561a966a01360f5db6675b101592
opacitySun <809340810@qq.com>
2016-08-02 11:35:47 +0800

    更新修改

--- public/javascripts/app/controller/c-index.js
+++ public/javascripts/app/controller/c-index.js
@@ -186,7 +186,7 @@ define(["./Base","jquery","fnbase","../model/m-index"],function(Base,$,fnbase,mo
 					$("#bannerForm").append(imgHtml);
 					$("#bannerSubmit").remove();
 					$("#bannerReset").remove();
-					$("#bannerForm").append("<button type="button" id="pageBack" class="btn btn-info">返回</button>");
+					$("#bannerForm").append('<button type="button" id="pageBack" class="btn btn-info">返回</button>');
 	            }else{
 	                alert("查找失败");
 	            }
