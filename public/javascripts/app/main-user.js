f11cf94a2c748b2c9b2aa142b3b4daf6c7e6f2bf
opacitySun <809340810@qq.com>
2016-08-09 16:21:40 +0800

    更新修改

--- public/javascripts/app/main-user.js
+++ public/javascripts/app/main-user.js
@@ -1,11 +1,11 @@
 define(['jquery','fnbase','bootstrap','./controller/c-user','./model/m-user'],function($,fnbase,bootstrap,controller,model){
     var urlPath = fnbase.getRouterName();
-	if(urlPath == "/user"){	//banner管理
+	if(urlPath == "/user"){	//用户管理
 		controller.getUserList();
 		$("#addUserButton").on("click",function(){
 			window.location.href = "/user-edit?type=add";
 		});	
-	}else if(urlPath == "/user-edit"){	//banner编辑
+	}else if(urlPath == "/user-edit"){	//用户编辑
 		var requestGet = fnbase.GetRequest();
 		var urlType = requestGet["type"];
         var urlId = requestGet["id"];
@@ -17,11 +17,11 @@ define(['jquery','fnbase','bootstrap','./controller/c-user','./model/m-user'],fu
         }else if(urlType == "edit_password"){
         	controller.editPassword(urlId);
         	$("#pageBack").on("click",function(){
-        		window.location.href="/banner";
+        		window.location.href="/user";
         	});
 			$("#pageHeader").html("用户 <small>修改密码</small>");
         }
-	}else if(urlPath == "/user-info-edit"){	//banner编辑
+	}else if(urlPath == "/user-info-edit"){	//用户信息编辑
 		var requestGet = fnbase.GetRequest();
         var urlId = requestGet["id"];
         controller.editUserInfo(urlId);
