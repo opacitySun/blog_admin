6e645811bc7580f21f93e5c34aadb912d4c902e0
opacitySun <809340810@qq.com>
2016-08-17 10:08:04 +0800

    更新修改

--- public/javascripts/app/main-page.js
+++ public/javascripts/app/main-page.js
@@ -1,6 +1,6 @@
 define(['jquery','fnbase','./controller/c-page','./model/m-page'],function($,fnbase,controller,model){
 	var pageSize = 15,buttons = 10;
 	var total = 150;
-	var func = function(){alert(1);};
+	var func = function(num){alert(num);};
     controller.createPage(pageSize,buttons,total,func);
 });
