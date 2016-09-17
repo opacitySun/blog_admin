1c9c2abe35787b97c17f0e783854f1e3302e0701
opacitySun <809340810@qq.com>
2016-09-17 18:56:43 +0800

    更新修改

--- server/controller/ueditorController.js
+++ server/controller/ueditorController.js
@@ -9,13 +9,18 @@ exports.removeImgAction = function(req, res) {
     var result = {};
     var file = req.body.path;
     file = file.replace(/\/\//g,"/");
-    if(file){
-        fs.unlinkSync('./public'+file);
-        result['state'] = 'success';
-        result['message'] = '删除完成';
-    }else{
+    try{
+        if(file){
+            fs.unlinkSync('./public'+file);
+            result['state'] = 'success';
+            result['message'] = '删除完成';
+        }else{
+            result['state'] = 'error';
+            result['message'] = '删除失败，未找到'+file;
+        }
+    }catch(err){
         result['state'] = 'error';
-        result['message'] = '删除失败，未找到'+file;
+        result['message'] = '删除失败：'+err;
     }
     res.json(result);
 } 
